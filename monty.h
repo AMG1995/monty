4037100f9f81d26c86145445f3e268b82616cb66
AMG1995 <amjed1996@gmail.com>
2023-11-16 20:29:32 +0000

    hi

--- monty.h
+++ monty.h
@@ -7,6 +7,15 @@
 #include <fcntl.h>
 #include <string.h>
 #include <ctype.h>
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
 typedef struct stack_s
 {
 	int n;
@@ -28,7 +37,15 @@ typedef struct bus_s
 	char *content;
 	int lifi;
 }  bus_t;
-
+extern bus_t bus;
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
 typedef struct instruction_s
 {
 	char *opcode;
@@ -59,3 +76,4 @@ void addqueue(stack_t **head, int n);
 void f_queue(stack_t **head, unsigned int counter);
 void f_stack(stack_t **head, unsigned int counter);
 #endif
+
