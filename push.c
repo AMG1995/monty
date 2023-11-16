0d67638cd0aa444c34248abe1f1ecf2cfd6ab974
AMG1995 <amjed1996@gmail.com>
2023-11-16 20:24:28 +0000

    hi

--- push.c
+++ push.c
@@ -10,43 +10,43 @@
  */
 void push_operation(stack_t **stack_head, unsigned int line_number)
 {
-    int value, index = 0, flag = 0;
+	int value, index = 0, flag = 0;
 
-    if (bus.argument)
-    {
-        if (bus.argument[0] == '-')
-            index++;
+	if (bus.argument)
+	{
+		if (bus.argument[0] == '-')
+		index++;
 
-        while (bus.argument[index] != '\0')
-        {
-            if (bus.argument[index] > 57 || bus.argument[index] < 48)
-                flag = 1;
-            index++;
-        }
+		while (bus.argument[index] != '\0')
+		{
+			if (bus.argument[index] > 57 || bus.argument[index] < 48)
+			flag = 1;
+			index++;
+		}
 
-        if (flag == 1)
-        {
-            fprintf(stderr, "L%d: usage: push integer\n", line_number);
-            fclose(bus.file);
-            free(bus.content);
-            free_stack(*stack_head);
-            exit(EXIT_FAILURE);
-        }
-    }
-    else
-    {
-        fprintf(stderr, "L%d: usage: push integer\n", line_number);
-        fclose(bus.file);
-        free(bus.content);
-        free_stack(*stack_head);
-        exit(EXIT_FAILURE);
-    }
+		if (flag == 1)
+		{
+			fprintf(stderr, "L%d: usage: push integer\n", line_number);
+			fclose(bus.file);
+			free(bus.content);
+			free_stack(*stack_head);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+	fprintf(stderr, "L%d: usage: push integer\n", line_number);
+	fclose(bus.file);
+	free(bus.content);
+	free_stack(*stack_head);
+	exit(EXIT_FAILURE);
+	}
 
-    value = atoi(bus.argument);
+	value = atoi(bus.argument);
 
-    if (bus.lifo == 0)
-        add_node(stack_head, value);
-    else
-        add_queue(stack_head, value);
+	if (bus.lifo == 0)
+		add_node(stack_head, value);
+	else
+		add_queue(stack_head, value);
 }
 
