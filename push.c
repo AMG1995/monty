81a954f77f0b4768a2e433be62a5476778556126
AMG1995 <amjed1996@gmail.com>
2023-11-18 08:21:52 +0000

    hi

--- push.c
+++ push.c
@@ -1,46 +1,41 @@
 #include "monty.h"
 
-/**
- * f_push - add node to the stack
- * @head: stack head
- * @counter: line_number
- * Return: no return
- */
-void f_push(stack_t **head, unsigned int counter)
+void f_push(stack_t **stack_head, unsigned int line_number)
 {
-	int n, j = 0, flag = 0;
+    int value, index = 0, invalid_flag = 0;
 
-	if (bus.arg)
-	{
-		if (bus.arg[0] == '-')
-			j++;
-		while (bus.arg[j] != '\0')
-		{
-			if (bus.arg[j] > 57 || bus.arg[j] < 48)
-				flag = 1;
-			j++;
-		}
-		if (flag == 1)
-		{
-			fprintf(stderr, "L%d: usage: push integer\n", counter);
-			fclose(bus.file);
-			free(bus.content);
-			free_stack(*head);
-			exit(EXIT_FAILURE);
-		}
-	}
-	else
-	{
-		fprintf(stderr, "L%d: usage: push integer\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	n = atoi(bus.arg);
-	if (bus.lifi == 0)
-		addnode(head, n);
-	else
-		addqueue(head, n);
+    if (new_bus.arg)
+    {
+        if (new_bus.arg[0] == '-')
+            index++;
+        while (new_bus.arg[index] != '\0')
+        {
+            if (new_bus.arg[index] > 57 || new_bus.arg[index] < 48)
+                invalid_flag = 1;
+            index++;
+        }
+        if (invalid_flag == 1)
+        {
+            fprintf(stderr, "L%d: usage: push integer\n", line_number);
+            fclose(new_bus.file);
+            free(new_bus.content);
+            free_stack(*stack_head);
+            exit(EXIT_FAILURE);
+        }
+    }
+    else
+    {
+        fprintf(stderr, "L%d: usage: push integer\n", line_number);
+        fclose(new_bus.file);
+        free(new_bus.content);
+        free_stack(*stack_head);
+        exit(EXIT_FAILURE);
+    }
+
+    value = atoi(new_bus.arg);
+
+    if (new_bus.lifo == 0)
+        add_node(stack_head, value);
+    else
+        add_queue(stack_head, value);
 }
-
