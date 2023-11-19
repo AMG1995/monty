29f7c4c22d508f6e3ac7e26ed68e6a5c04455f6f
AMG1995 <amjed1996@gmail.com>
2023-11-19 07:39:11 +0000

    hi

--- print_str.c
+++ print_str.c
@@ -1,19 +1,18 @@
 #include "monty.h"
 
 /**
- * f_print_string - prints the ASCII characters from the stack
- * @stack_top: pointer to the top node of the stack
- * @line_number: line number in the script (unused in this function)
+ * f_pstr - print string at the top of stack
+ * @head: first node of linked list
+ * @counter: line counter
  *
  * Return: none
  */
-void f_print_string(stack_t **stack_top, unsigned int line_number)
+void f_pstr(stack_t **head, unsigned int counter)
 {
 	stack_t *current_node;
+	(void)counter;
 
-	(void)line_number;
-
-	for (current_node = *stack_top; current_node; current_node = current_node->next)
+	for (current_node = *head; current_node; current_node = current_node->next)
 	{
 		if (current_node->n > 127 || current_node->n <= 0)
 		{
